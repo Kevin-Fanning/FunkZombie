4466ae18c721e0599005cab8eb6631170383cdab
Kevin Fanning <kmf5302@gmail.com>
2013-03-01 18:03:04 -0500

    Destroy old temp objects that shouldn't have been here

--- playground/Resource.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "Resource.h"
-
-Resource::Resource(std::string name) : m_name(name)
-{
-	std::transform(m_name.begin(), m_name.end(), m_name.begin(), (int(*)(int))std::tolower);
-}
